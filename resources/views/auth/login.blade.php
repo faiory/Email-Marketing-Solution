16ac401472d3464759c8d3ef43c5f4a182d138b5
Faisal <201500460@student.polytechnic.bh>
2019-03-24 09:28:55 +0300

    Layout
    Layout now working perfectly

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,6 @@
-@extends('layouts.app') 
-@section('content')
-{{--  --}}
-<!DOCTYPE html>
-<html>
-
-<head>
+ {{-- @extends('authLayout') --}}
+ 
+ <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>TitleHere</title>
@@ -16,7 +12,8 @@
   <link rel="stylesheet" href="bower_components/admin-lte/dist/css/skins/_all-skins.min.css">
 </head>
 
-<body class="hold-transition skin-black sidebar-mini">
+{{-- @section('content') --}}
+<div class="hold-transition skin-black sidebar-mini">
   <div class="login-box">
     <div class="login-logo">
       <a href=""><b class="bg-maroon">Maroon</b>Frog</a>
@@ -27,11 +24,9 @@
 
       <form method="POST" action="{{ route('login') }}">
         @csrf
-
         <div class="form-group has-feedback">
-          {{-- <input type="email" class="form-control" placeholder="Email"> --}}
-          <input id="email" type="email" placeholder="Email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}"
-            required autofocus>
+          <input id="email" type="email" placeholder="Email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email"
+            value="{{ old('email') }}" required autofocus>
           <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
         </div>
         @if ($errors->has('email'))
@@ -43,12 +38,10 @@
         <div class="form-group has-feedback">
           <input name="password" id="password" placeholder="Password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
             placeholder="Password">
-          <span class="glyphicon glyphicon-lock form-control-feedback"></span> 
-          @if ($errors->has('password'))
+          <span class="glyphicon glyphicon-lock form-control-feedback"></span> @if ($errors->has('password'))
           <span class="invalid-feedback" role="alert">
             <strong>{{ $errors->first('password') }}</strong>
-          </span> 
-          @endif
+          </span> @endif
         </div>
 
         <div class="form-group row mb-0">
@@ -64,74 +57,13 @@
       </div>
       <!-- /.login-box-body -->
     </div>
-    @endsection
+  </div>
+  <script src="bower_components/jquery/dist/jquery.min.js"></script>
+  <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  <script src="bower_components/admin-lte/dist/js/adminlte.min.js"></script>
+{{-- @stop --}}
 
-    <script src="bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="bower_components/admin-lte/dist/js/adminlte.min.js"></script>
-</body>
 
-</html>
-{{--  --}}
-{{-- <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}"
-                                    required autofocus> @if ($errors->has('email'))
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span> @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password"
-                                    required> @if ($errors->has('password'))
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span> @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old( 'remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button> @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a> @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> --}}
-{{-- @endsection --}}
+  
